c6df42ba32c2bef284fcbd244f2576296018b25d
jobiols <jorge.obiols@gmail.com>
2017-12-26 11:57:54 -0300

    [ADD] bajar factura electronica desde POS

--- iomaq_default/models/__init__.py
+++ iomaq_default/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import pos_invoice
+from . import report_controller
---
+++ iomaq_default/models/report_controller.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from openerp.addons.web.http import Controller, route, request
+from openerp.addons import report
+
+
+class ReportControllerDerived(report.controllers.main.ReportController):
+
+    @route(['/report/download'], type='http', auth="user")
+    def report_download(self, data, token):
+
+        # le cambiamos el reporte para que salga el de aeroo
+        data = data.replace('point_of_sale.report_invoice',
+                            'aeroo_report_ar_einvoice')
+
+        ret = super(ReportControllerDerived, self).report_download(data, token)
+        return ret
